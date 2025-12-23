e2a76e55fa9f462af22df9a354d8fe4f0e0397c8
LCT5301A <telio.lacome@etu.iut-tlse3.fr>
2025-12-23 13:12:03 +0100

    working dark mode

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,23 +1,72 @@
 <script setup lang="ts">
-const [mode, switchMode] = useToggle("Dark Mode", {
-    truthyValue: "Dark Mode",
-    falsyValue: "Light Mode",
+const isDark = useDark()
+
+const toggleDarkMode = useToggle(isDark)
+
+const darkModeText = computed(() => {
+    return isDark.value ? "Dark Mode" : "Light Mode"
 })
+
+const darkModeIconName = computed(() =>
+    isDark.value
+        ? "i-line-md:sunny-outline-to-moon-loop-transition"
+        : "i-line-md:moon-to-sunny-outline-loop-transition"
+)
 </script>
 
 <template>
     <div class="">
-        <header class="flex justify-between py-8 px-5 shadow-md font-display red">
-            <p class="font-bold red">Where in the world?</p>
-            <div @click="switchMode()">
-                <!-- <img src="" /> -->
-                <p>{{ mode }}</p>
+        <header
+            class="relative flex justify-between py-8 px-5 shadow-md font-display dark:bg-blue-900"
+        >
+            <p class="font-extrabold dark:text-white">Where in the world?</p>
+            <div
+                class="relative flex items-center justify-between w-[6.5rem] cursor-pointer select-none"
+                @click="toggleDarkMode()"
+            >
+                <Icon
+                    :key="darkModeIconName"
+                    :name="darkModeIconName"
+                    mode="svg"
+                    restart="always"
+                    :color="isDark ? 'white' : 'black'"
+                />
+                <Transition>
+                    <p
+                        :key="darkModeText"
+                        class="absolute right-0 font-medium dark:text-white"
+                    >
+                        {{ darkModeText }}
+                    </p>
+                </Transition>
             </div>
         </header>
         <slot />
     </div>
 </template>
 
-<style scoped>
+<style>
+body {
+    @apply bg-gray-50 dark:bg-blue-950 dark:text-white;
+}
+</style>
 
+<style scoped>
+.v-enter-active {
+    transition: all .5s ease;
+}
+
+.v-leave-active {
+    transition: transform .5s ease, opacity .3s ease-out;
+}
+
+.v-enter-from {
+    transform: translateY(-15px);
+    opacity: 0;
+}
+
+.v-leave-to {
+    transform: translateY(15px);
+    opacity: 0;
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxtjs/tailwindcss',
-    '@vueuse/nuxt',
-    '@nuxt/icon',
-    '@nuxt/fonts'
-  ]
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    modules: [
+        "@nuxt/eslint",
+        "@nuxt/image",
+        "@nuxtjs/tailwindcss",
+        "@vueuse/nuxt",
+        "@nuxt/icon",
+        "@nuxt/fonts",
+    ],
+    icon: {
+        mode: "css",
+        cssLayer: "base",
+    },
+    fonts: {
+        defaults: {
+            weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        },
+    },
 })
